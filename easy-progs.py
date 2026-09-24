d89a1cf648770d6f81e3cd788647ac57650c69b3
angelalarionova <angelalarionova13@gmail.com>
2026-09-24 09:59:01 +0700

    добавила скидку при покупке > 5 кг

--- easy-progs.py
+++ easy-progs.py
@@ -1,4 +1,6 @@
 count = int(input("введите кол-во товара: "))
 price = 158.0
+if count > 5:
+    price = 150.0
 name = 'картошка'
 print(f'{name}, стоит {price} за килограмм, вы набрали {count}кг, общая сумма {count*price} рублей')
